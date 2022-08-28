c0861d28b1e0caad53dcb85fefb8f95dbfd7b1c2
AMD <michojekunle1@gmail.com>
2022-08-28 13:59:56 +0100

    working on the register route

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:process.env.REACRT_APP_API_KEY ,
-  authDomain:process.env.REACRT_APP_AUTH_DOMAIN ,
-  databaseURL:process.env.REACRT_APP_DATABASE_URL ,
-  projectId:process.env.REACRT_APP_PROJECT_ID ,
-  storageBucket:process.env.REACRT_APP_STORAGE_BUCKET ,
-  messagingSenderId:process.env.REACRT_APP_MESSAGING_SENDER_ID ,
-  appId:process.env.REACRT_APP_APP_ID ,
-  measurementId:process.env.REACRT_APP_MESSAGING_ID, 
+  apiKey: "AIzaSyCXyGDWEtGL7sDjGDdSuzAHydKkjNcQyAM",
+  authDomain: "test-project-fc23b.firebaseapp.com",
+  databaseURL: "https://test-project-fc23b-default-rtdb.firebaseio.com",
+  projectId: "test-project-fc23b",
+  storageBucket: "test-project-fc23b.appspot.com",
+  messagingSenderId: "87127082698",
+  appId: "1:87127082698:web:4d8d271790b5744bbd1633",
+  measurementId: "G-1QRW4JE4E6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,57 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import { setDoc, doc } from 'firebase/firestore'
 
 
 const Register = () => {
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: null,
+        loading: false
+    });
+
+    const {name, email, password, error, loading} = data;
+
+    const handleChange = e => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        if(!name || !password || !email) {
+            setData({...data, error: "All fields Are required!"})
+        }
+        try {
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(result.user);
+            console.log(data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
   return (
     <div>
         <section>
             <h3>Create An Account</h3>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="input_container">
                     <label htmlFor="name">Name</label>
-                    <input type="text" name='name' />
+                    <input type="text" name='name' value={name} onChange={handleChange}/>
                 </div>
                 <div className="input_container">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name='email' />
+                    <input type="email" name='email' value={email} onChange={handleChange}/>
                 </div>
                 <div className="input_container">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name='password' />
+                    <input type="password" name='password' value={password} onChange={handleChange}/>
                 </div>
+                {error ? <p className='error'>{error}</p>: ''}
                 <div className="buttons">
                     <button type="reset">Clear</button>
                     <button type="submit">Register</button>
